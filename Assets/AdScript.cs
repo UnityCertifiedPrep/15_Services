b58f068746a5de509aeff789aed873ed14ed7290
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-26 12:15:42 +0100

    Setup ads

---
+++ Assets/AdScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+
+public class AdScript : MonoBehaviour {
+
+    bool hasShownAdd = false;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!hasShownAdd)
+        {
+            if (Advertisement.IsReady())
+            {
+                Advertisement.Show();
+                hasShownAdd = true;
+            }
+        }
+    }
+}
